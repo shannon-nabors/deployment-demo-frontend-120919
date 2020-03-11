071707b0a90279e21c69641f52b5371f0554ecb9
Shannon Nabors <shannon.nabors@gmail.com>
2020-03-11 16:37:01 -0400

    adding content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {flags: []}
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/flags')
+    .then(res => res.json())
+    .then(flags => this.setState({flags: flags}))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header">My Favorite Flags</div>
+        <div className="gallery">
+          {this.state.flags.map(flag => {
+            return (
+              <div key={flag.id} className="card">
+                <img src={flag.image}></img>
+                <p>{flag.country}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
